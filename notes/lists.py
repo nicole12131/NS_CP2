f166e9e0b85d189207e44eb26a71bf2b0c4e1331
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-01-07 09:51:39 -0700

    decorator

--- notes/lists.py
+++ notes/lists.py
@@ -1,1 +1,21 @@
-# NS 1st Lists Notes 
+# NS 1st 
+
+def decorator(func):
+    def wrapper():
+        print("Before calling the function.")
+        func()
+        print("after calling the function")
+    return wrapper
+
+@decorator
+def greet():
+    print("hello, world ")
+
+greet()
+
+@decorator
+def add():
+    print(1+1)
+
+
+add()
