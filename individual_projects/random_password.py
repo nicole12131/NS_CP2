248cacfb5ec821b75583d6256bfd48ec21325209
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-01-22 09:51:44 -0700

    random password

---
+++ individual_projects/random_password.py
@@ -0,0 +1,35 @@
+# NS 1st Random password generator 
+
+# define main function 
+def main():
+# Ask the user to type the number for the action that they like to perform
+# 1. Generate Passwords
+# 2. Exit
+
+    print("1. Generate password ")
+print("2. Exit ")
+action = input("Type the number for the action you would like to perform: ")
+
+# if user choose 1
+# define function for how long the password needs to be 
+# Ask user how long the password need to be
+
+def password_():
+    if action == "1":
+        l = input("How long does the password needs to be: ")
+
+# define function for lowercase letters in password 
+# Ask if the password needs lowercase letters
+
+# define function for uppercase letters in password 
+# Ask if the password needs uppercase letters
+
+# define a function for numbers in password 
+# Ask if the password needs numbers
+
+# define function for special characters in password 
+# Ask if password needs special characters 
+# print possible passwords
+
+# if user choose 2 
+# the random generator will stop 
