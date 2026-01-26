4e20837e91cc6d2949c6f3adbdba25a8a83062ce
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-01-26 09:06:39 -0700

    password

--- individual_projects/random_password.py
+++ individual_projects/random_password.py
@@ -1,35 +1,65 @@
 # NS 1st Random password generator 
 
 # define main function 
-def main():
+
 # Ask the user to type the number for the action that they like to perform
 # 1. Generate Passwords
 # 2. Exit
 
+def main(): 
     print("1. Generate password ")
-print("2. Exit ")
-action = input("Type the number for the action you would like to perform: ")
+    print("2. Exit ")
+    action = input("Type the number for the action you would like to perform: ")
 
 # if user choose 1
-# define function for how long the password needs to be 
 # Ask user how long the password need to be
-
-def password_():
-    if action == "1":
-        l = input("How long does the password needs to be: ")
-
-# define function for lowercase letters in password 
 # Ask if the password needs lowercase letters
-
-# define function for uppercase letters in password 
 # Ask if the password needs uppercase letters
-
-# define a function for numbers in password 
 # Ask if the password needs numbers
-
-# define function for special characters in password 
 # Ask if password needs special characters 
-# print possible passwords
+
+    if action == "1":
+        length = input("How long does the password needs to be: ")
+        lower = input("Does the password need lowercase letters (Y/N): ")
+        upper = input("Does the password need uppercase letters (Y/N): ")
+        num = input("Does the password need numbers letters (Y/N): ")
 
 # if user choose 2 
-# the random generator will stop 
+# print thanks for using the program 
+
+    if action == "2":
+        print(" thank you for using this pogram bye bye ")
+# define function for how long the password needs to be
+
+    def password_length():
+
+# define function for lowercase letters in password 
+
+    def password_lowecase():
+
+
+
+
+# define function for uppercase letters in password 
+
+# define a function for numbers in password 
+
+# define function for special characters in password 
+
+# print possible passwords
+
+main()
+
+
+    
+   
+    
+
+
+
+
+
+
+
+
+
