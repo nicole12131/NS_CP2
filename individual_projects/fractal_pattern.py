630cd53d52e7c0f429b87e7435f706cf588ced65
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-02-27 09:52:34 -0700

    fractal

---
+++ individual_projects/fractal_pattern.py
@@ -0,0 +1,23 @@
+# NS 1st Fractal Pattern Generator
+import turtle
+
+depth = input("Enter recursion depth(1-5): ")
+screen = turtle.Screen
+t = turtle.Turtle(shape="turtle")
+t.penup()
+t.goto(-300, -300)
+t.pendown()
+t.left(60)
+t.forward(600)
+t.right(120)
+t.forward(600)
+t.right(120)
+t.forward(600)
+t.right(120)
+t.forward(300)
+t.right(60)
+t.forward(300)
+t.right(120)
+t.forward(300)
+t.right(120)
+t.forward(300)
