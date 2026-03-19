7b57b7de903998fcad16938035adf3ab9bf0cf7e
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-03-19 09:51:46 -0600

    pet simulator

---
+++ pet_simulator/feeding_menu.py
@@ -0,0 +1,1 @@
+def feeding_menu():
--- pet_simulator/main.py
+++ pet_simulator/main.py
@@ -1,5 +1,16 @@
 # NS 1st Pet simulator 
+from pet_creation import pet_creation
 print("=====================================")
 print("🐾 VIRTUAL PET SIMULATOR 🐾")
 print("=====================================")
 
+print("🎮 ACTIONS: \n[1] Feed Pet\n[2] Play with Pet\n[3] Put Pet to Sleep\n[4] Check Status\n[5] Pet Management\n[6] Save Game\n[7] Load Game\n[8] Quit")
+choice = input("Enter your choice (1-8): ")
+
+if choice == "1":
+
+elif choice == "2":
+
+elif choice == "3"
+
+elif choice == "4"
