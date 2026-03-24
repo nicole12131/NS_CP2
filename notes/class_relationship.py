9b0fb0239a7436467679f70767ff52df3ab5b798
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-03-24 09:50:48 -0600

    class relationship notes!

---
+++ notes/class_relationship.py
@@ -0,0 +1,69 @@
+# NS 1st Class Relationship Notes
+
+# Inheritance "is a"
+# Parent Class
+class Vehical:
+    def __init__(self, model, brand):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Move!")
+
+# Child Class
+class Car(Vehical):
+    pass
+
+class Boat(Vehical):
+    def move(self):
+        print("Sail!") 
+
+class Plane(Vehical):
+    def move(self):
+        print("Fly!")
+
+car = Car("Ford", "Mustang")
+boat = Boat("Ibiza", "Touring 20")
+plane = Plane("Boeing", "747")
+
+car.move()
+boat.move()
+plane.move()
+
+
+# Aggregation 
+class Library:
+    def __init__(self, name, catalog = []):
+        self.name = name 
+        self.catalog = catalog
+
+    def add_book(self, book):
+        self.catalog.append(book)
+
+    def remove_book(self, book):
+        if book is self.catalog:
+            self.catalog.pop(book)
+        else:
+            print("That book isn't in this library")
+
+    def view_catalog(self):
+        for book in self.catalog:
+            print(book)
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title.title()
+        self.author = author
+
+def __str__(self):
+    return f"{self.title} by {self.author}"
+
+
+lib = Library("Provo Library")
+
+lib.add_book(Book("Way of Kings", "Brandon Sanderson"))
+lib.add_book(Book("Fellowship of the Ring", "J.R.R. Tolkien"))
+lib.add_book(Book("The last Battle", "C.S. Lewis"))
+
+lib.view_catalog()
+
