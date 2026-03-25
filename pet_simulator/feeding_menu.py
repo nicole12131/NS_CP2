386f6f26ce082cbd94927e238dbd0a40577014b4
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-03-24 22:03:10 -0600

    Delete pet_simulator directory

--- pet_simulator/feeding_menu.py
+++
@@ -1,1 +0,0 @@
-def feeding_menu():
--- pet_simulator/pet_creation.py
+++
@@ -1,9 +0,0 @@
-# NS 1st pet creation 
-
-print("Welcome! Let's create your first pet!")
-def pet_creation():
-    name = input("Pet name: ")
-    specie = input("species : ")
-    age = input("Age: ")
-    print("Pet created successfully! 🎉")
-pet_creation()
