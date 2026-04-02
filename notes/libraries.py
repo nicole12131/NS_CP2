9d076be779c1882bd37fd3c3eeec02b2b4926667
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-04-02 09:51:56 -0600

    hi

--- notes/libraries.py
+++ notes/libraries.py
@@ -1,5 +1,20 @@
-import numpy as np
-a = np.arange(15).reshape(3,5)
-print(a.shape)
-print(a.ndim)
-print(a.dytype.name)
+import turtle
+
+def turn(i):
+    left = (((i & -i) << 1) & i) != 0
+    return 'L' if left else 'R'
+
+def curve(iteration):
+    return ''.join([turn(i + 1) for i in range(2 ** iteration - 1)])
+
+if __name__ == '__main__':
+    turtle.showturtle()
+    turtle.hideturtle()
+    turtle.speed(0)
+    i = 1
+    while True:
+        if turn(i) == 'L':
+            turtle.circle(-4, 90, 36)
+        else:
+            turtle.circle(4, 90, 36)
+        i += 1
